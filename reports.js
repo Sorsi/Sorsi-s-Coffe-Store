c9cc24eb7bf28b8a634845ddbee9fb9f220b64bf
Orsolya <orsolya.suto@ericsson.com>
2020-09-28 14:26:57 +0200

    correcting reports.js chart

--- reports.js
+++ reports.js
@@ -1,17 +1,17 @@
 /*jshint esversion: 6 */
 let coffeeSales = [
-  { month: Jan, sales: 170 },
-  { month: Feb, sales: 320 },
-  { month: Mar, sales: 432 },
-  { month: Apr, sales: 548 },
-  { month: May, sales: 342 },
-  { month: Jun, sales: 689 },
-  { month: Jul, sales: 344 },
-  { month: Aug, sales: 109 },
-  { month: Sep, sales: 655 },
-  { month: Oct, sales: 327 },
-  { month: Nov, sales: 109 },
-  { month: Dec, sales: 235 },
+  { month: "Jan", sales: 45 },
+  { month: "Feb", sales: 65 },
+  { month: "Mar", sales: 72 },
+  { month: "Apr", sales: 80 },
+  { month: "May", sales: 60 },
+  { month: "Jun", sales: 99 },
+  { month: "Jul", sales: 70 },
+  { month: "Aug", sales: 109 },
+  { month: "Sep", sales: 97 },
+  { month: "Oct", sales: 67 },
+  { month: "Nov", sales: 109 },
+  { month: "Dec", sales: 50 },
 ];
 
 function createAxis(context, startX, startY, endX, endY) {
@@ -34,18 +34,14 @@ function drawChart() {
   let canvas = document.getElementById("bar-chart");
   if (canvas && canvas.getContext) {
     let context = canvas.getContext("2d");
-    console.log(canvas.height);
-    createBarChart(context, coffeeSales, 30, 20, canvas.height - 20, 50);
+    createBarChart(context, coffeeSales, 30, 6, canvas.height - 20, 50);
   }
 }
 
 function createBarChart(context, data, startX, barWidth, chartHeight) {
-  console.log("hhhhhhhhhhhhhhhhhhhhhh");
   context.lineWidth = "1.2";
-  let startY = 780;
-  console.log(startY, "startjfjfjfjf");
-  createAxis(context, startX, startY, startX, 30); //vertical
-  createAxis(context, startX, startY, startY, 650, startY); //horizontal
+  createAxis(context, startX, chartHeight, startX, 30); //vertical
+  createAxis(context, startX, chartHeight, 350, chartHeight); //horizontal
 
   context.lineWidth = "0.0";
   let maxSales = 0;
@@ -54,19 +50,23 @@ function createBarChart(context, data, startX, barWidth, chartHeight) {
     let itemSales = data[i].sales;
     if (itemSales > maxSales) maxSales = itemSales;
 
-    context.fillStyle = "blue";
+    context.fillStyle = "green";
     createBar(
       context,
-      20 + startX + i * barWidth + i + i * 30,
-      chartHeight - itemSales
+      10 + startX + i * barWidth + i + i * 20,
+      chartHeight - itemSales,
+      barWidth,
+      itemSales,
+      true
     );
 
     context.textAlign = "left";
     context.fillStyle = "black";
     context.fillText(
       itemMonth,
-      20 + startX + i * barWidth + i + i * 30,
-      chartHeight + 15
+      10 + startX + i * barWidth + i + i * 20,
+      chartHeight + 15,
+      200
     );
   }
 }
